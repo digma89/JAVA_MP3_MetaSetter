5420adfdc0001d1e0e01ce5cd3062cd2ab3e4029
digma89 <digma89@hotmail.com>
2016-10-19 16:58:45 -0400

    First commit set Meta

---
+++ SetFileNamesMP3/src/main/BackEnd.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.mpatric.mp3agic.BaseException;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.Mp3File;
+
+public class BackEnd {
+	
+	//Method that loops in directory and get files names
+	public void getFiles(String folerSource, String folderResult) throws BaseException, IOException, Exception{
+		File folder = new File(folerSource);
+		File[] listOfFiles = folder.listFiles();
+		    for (int i = 0; i < listOfFiles.length; i++) {
+		      if (listOfFiles[i].isFile()) {
+		    	 
+		     	//send file name && folder result to next method			        
+		        setMetaData(listOfFiles[i].getName().trim(), folerSource, folderResult);	
+		      }
+		    }
+	}
+	
+	//With the file name set the MetaData of the files
+	private void setMetaData(String fileName, String folerSource, String folderResult) throws BaseException, Exception, IOException{		
+        if(fileName.endsWith(".mp3") && fileName != "" && !fileName.equals(null)){
+	        
+        	//Local variables
+        	String artistName;
+	        String title;
+	        
+	        artistName = fileName.substring(0, fileName.indexOf("-")).trim();
+	        title = fileName.substring(fileName.indexOf("-")+1,fileName.indexOf(".mp3") ).trim();	 
+	        
+	        //Open mp3 file
+	        Mp3File mp3file = new Mp3File(folerSource +fileName);	        
+			if (mp3file.hasId3v2Tag()) {
+				 ID3v2 id3v2Tag = mp3file.getId3v2Tag();			 
+				 id3v2Tag.setArtist(artistName);	
+				 id3v2Tag.setTitle(title);
+				 mp3file.save(folderResult +fileName);
+				 System.out.println("Complete for: " + fileName);
+			}               
+        }
+	}
+
+}
---
+++ SetFileNamesMP3/src/main/MainApp.java
@@ -0,0 +1,196 @@
+package main;
+
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.JFrame;
+
+import com.mpatric.mp3agic.BaseException;
+import com.mpatric.mp3agic.ID3v1;
+import com.mpatric.mp3agic.ID3v2;
+import com.mpatric.mp3agic.ID3v22Tag;
+import com.mpatric.mp3agic.InvalidDataException;
+import com.mpatric.mp3agic.Mp3File;
+import com.mpatric.mp3agic.UnsupportedTagException;
+
+import javax.swing.JPanel;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLayeredPane;
+import javax.swing.JToolBar;
+import javax.swing.JDesktopPane;
+import javax.swing.JTabbedPane;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JFileChooser;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JLabel;
+
+
+
+public class MainApp {
+
+	private JFrame frame;
+	private JTextField textField;
+	
+	//Create global labels to set after click events
+	JLabel lbl_setMeta_sourceFolder = new JLabel("");
+	JLabel lbl_setMeta_resultfolder = new JLabel("");
+	private JTextField tf_setMeta_album;
+	private JTextField tf_setMeta_year;
+	private JTextField tf_setMeta_genre;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					MainApp window = new MainApp();					
+					window.frame.setVisible(true);
+					
+					BackEnd backend  = new BackEnd();
+					
+					backend.getFiles("C:/Users/Diego/Music/", "C:/Users/Diego/Music/Results/");			
+										
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainApp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 700, 297);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new CardLayout(0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		frame.getContentPane().add(tabbedPane, "name_347042451980677");
+		
+		JPanel setMeta = new JPanel();
+		tabbedPane.addTab("Set metaData", null, setMeta, null);
+		setMeta.setLayout(null);		
+		
+		//BTN select source folder
+		JButton btn_setMeta_sourceFolder = new JButton("Source folder");
+		btn_setMeta_sourceFolder.setBounds(12, 12, 109, 31);
+		setMeta.add(btn_setMeta_sourceFolder);
+		btn_setMeta_sourceFolder.addActionListener(new ActionListener() {			
+			public void actionPerformed(ActionEvent arg0) {
+				lbl_setMeta_sourceFolder.setText(fileChooser());
+			}
+		});
+		
+		
+		lbl_setMeta_sourceFolder.setBounds(133, 16, 532, 21);
+		setMeta.add(lbl_setMeta_sourceFolder);
+		
+		//BTN select result folder
+		JButton btn_setMeta_resultFolder = new JButton("Result folder");		
+		btn_setMeta_resultFolder.setBounds(12, 56, 109, 31);
+		setMeta.add(btn_setMeta_resultFolder);
+		btn_setMeta_resultFolder.addActionListener(new ActionListener() {			
+			public void actionPerformed(ActionEvent arg0) {
+				lbl_setMeta_resultfolder.setText(fileChooser());		
+			}
+		});
+		
+		lbl_setMeta_resultfolder.setBounds(133, 63, 532, 21);
+		setMeta.add(lbl_setMeta_resultfolder);
+		
+		JButton btn_setMeta_run = new JButton("Run");
+		btn_setMeta_run.setBounds(556, 176, 109, 31);
+		setMeta.add(btn_setMeta_run);
+		
+		JLabel lbl_setMeta_album = new JLabel("Album name:");
+		lbl_setMeta_album.setBounds(12, 98, 109, 21);
+		setMeta.add(lbl_setMeta_album);
+		
+		tf_setMeta_album = new JTextField();
+		tf_setMeta_album.setBounds(133, 97, 265, 22);
+		setMeta.add(tf_setMeta_album);
+		tf_setMeta_album.setColumns(10);
+		
+		JLabel lbl_setMeta_year = new JLabel("Year:");
+		lbl_setMeta_year.setBounds(12, 133, 109, 21);
+		setMeta.add(lbl_setMeta_year);
+		
+		tf_setMeta_year = new JTextField();
+		tf_setMeta_year.setColumns(10);
+		tf_setMeta_year.setBounds(133, 132, 265, 22);
+		setMeta.add(tf_setMeta_year);
+		
+		JLabel lbl_setMeta_genre = new JLabel("Genre:");
+		lbl_setMeta_genre.setBounds(12, 168, 109, 21);
+		setMeta.add(lbl_setMeta_genre);
+		
+		tf_setMeta_genre = new JTextField();
+		tf_setMeta_genre.setColumns(10);
+		tf_setMeta_genre.setBounds(133, 167, 265, 22);
+		setMeta.add(tf_setMeta_genre);
+		
+		JPanel setFileName = new JPanel();
+		tabbedPane.addTab("Set file name", null, setFileName, null);
+		setFileName.setLayout(null);
+		
+		JButton button = new JButton("New button");
+		button.setBounds(200, 81, 219, 51);
+		setFileName.add(button);	 
+		
+		textField = new JTextField();
+		textField.setBounds(49, 144, 116, 22);
+		setFileName.add(textField);
+		textField.setColumns(10);
+	}	   
+	
+	
+	public String fileChooser(){
+		int result;
+	    JFileChooser chooser;
+	    chooser = new JFileChooser(); 
+	    chooser.setCurrentDirectory(new java.io.File("C:/"));
+	    chooser.setDialogTitle("choosertitle");
+	    chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	    //
+	    // disable the "All files" option.
+	    //
+	    chooser.setAcceptAllFileFilterUsed(false);
+	    //    
+	    if (chooser.showSaveDialog(chooser) == JFileChooser.APPROVE_OPTION) { 
+	      //System.out.println("getCurrentDirectory(): " +  chooser.getCurrentDirectory());
+	      //System.out.println("getSelectedFile() : " +  chooser.getSelectedFile());
+	      return chooser.getSelectedFile().toString();
+	      }
+	    else {
+	    	return "No selection";
+	      }
+	     }
+}
